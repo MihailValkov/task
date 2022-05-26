6a5f3b5f87bc76adb10bcac986fc859be340b143
mihailValkov <mihail_valko@mail.bg>
2022-05-26 14:26:05 +0300

    delete user-item component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-item/user-item.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserService } from './user.service';
 
 @NgModule({
-  declarations: [UserListComponent, UserItemComponent, UserDetailComponent],
+  declarations: [UserListComponent, UserDetailComponent],
   imports: [CommonModule, UserRoutingModule],
   providers: [UserService],
 })
